56157f8bfd1aaf0041835e511e916ca5e9a7dc83
pemba817 <58839852+pemba817@users.noreply.github.com>
2020-01-11 19:50:59 +0545

    "Crud Continue"

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path,include
 from .views import *
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns=[
     path('add-product',get_products)
 ]
+
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
